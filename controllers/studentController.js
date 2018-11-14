76f91ccd46440b8acd514e04b3b7512288cd923f
G4tlan <1043882@students.wits.ac.za>
2018-11-14 19:52:33 +0200

    database initialised

---
+++ controllers/studentController.js
@@ -0,0 +1,5 @@
+var db = require("../core/db")
+
+exports.getStudentBuildings = function(req,res){
+    db.exeSQL("select BuildingID from ")
+}
---
+++ settings.js
@@ -0,0 +1,9 @@
+exports.dbConfig = {
+    user: "Admin",
+    Password: "ADMIN",
+    server: ".",
+    database: "up_app",
+    port: 2000
+}
+
+exports.HTTPport = 2000
